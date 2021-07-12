81781d01d08ff4ede24445f26fe1798963593bcd
maldinipunisher <maldinipunisher@gmail.com>
2021-07-12 12:05:45 +0700

    v2.1

---
+++ Assets/Movement.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+public class Movement : MonoBehaviour
+{
+	private Vector3 walking;
+	[SerializeField]
+	[Range(0, 0.3f)]
+	private float walkingSpeed;
+	private float walkHor, walkVer;
+	public GameObject analog;
+
+	void Start()
+	{
+		transform.localEulerAngles = new Vector3(0, 150, 0);
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+
+		walkHor = Input.GetAxis("Horizontal") * walkingSpeed * Time.smoothDeltaTime;
+		walkVer = Input.GetAxis("Vertical") * walkingSpeed * Time.smoothDeltaTime;
+
+
+		walking = new Vector3(analog.transform.localPosition.x, 0, analog.transform.localPosition.y);
+
+		transform.Translate(walking * (walkingSpeed * Time.deltaTime));
+	}
+
+}
---
+++ README.md
@@ -0,0 +1,16 @@
+## MFPC for Unity
+Now you can make game more easier with this asset. this asset is for free you can download it on the top. 
+
+**Setup :**
+- Use FpsCamera.cs to your desired camera or player
+- Place TouchArea.cs to a canvas for area you can touch for controller (Camera view)
+- Place Movement.cs to your player
+
+Enjoy!
+
+**FIXED**
+- weird control 
+
+**View**
+
+![example project](https://github.com/maldinipunisher/MFPC-Unity/blob/main/demo.gif)

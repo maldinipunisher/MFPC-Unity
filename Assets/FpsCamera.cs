94c3437b9a64910bc7a49881650f494d0d123d6a
maldinipunisher <maldinipunisher@gmail.com>
2021-07-12 11:45:40 +0700

    v1.2

---
+++ Assets/FpsCamera.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+
+public class FpsCamera : MonoBehaviour
+{
+    public TouchArea fixedtouch; 
+    [Range(0f, 5f)]
+    public float Sensitivity;
+    //private Rigidbody rbPlayer;
+    private float lookVer, lookHor; 
+
+    void Start()
+    {
+        
+        //rbPlayer = GetComponent<Rigidbody>();
+        //Cursor.lockState = CursorLockMode.Locked; 
+
+    }
+
+    void Update()
+    {
+        if(gameObject.tag == "MainCamera")
+        {
+            LookUp();
+        }else if(gameObject.tag == "Player")
+        {
+            lookHorF(); 
+        }
+    }
+
+    void LookUp()
+    {
+        lookVer = fixedtouch.touchDist.y * Sensitivity; 
+        transform.localEulerAngles -= new Vector3(lookVer, 0, 0); 
+    }
+
+    void lookHorF()
+    {
+        lookHor += fixedtouch.touchDist.x * Sensitivity;
+
+        transform.localEulerAngles = new Vector3(0,lookHor, 0);
+    }
+}
---
+++ Assets/TouchArea.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems; 
+
+public class TouchArea : MonoBehaviour, IPointerDownHandler, IPointerUpHandler
+{
+    [HideInInspector]
+    public Vector2 touchDist;
+    [HideInInspector]
+    public Vector2 pointerOld;
+    [HideInInspector]
+    protected int pointerId;
+    [HideInInspector]
+    public bool pressed; 
+
+    void Update()
+    {
+        if (pressed)
+        {
+            if(pointerId >= 0 && pointerId < Input.touches.Length)
+            {
+                touchDist = Input.touches[pointerId].position - pointerOld;
+                pointerOld = Input.touches[pointerId].position; 
+            }
+            else
+            {
+                touchDist = new Vector2(Input.mousePosition.x, Input.mousePosition.y) - pointerOld;
+                pointerOld = Input.mousePosition; 
+            }
+        }
+        else
+        {
+            touchDist = new Vector2(); 
+        }
+    }
+
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        pressed = true;
+        pointerId = eventData.pointerId;
+        pointerOld = eventData.position; 
+    }
+
+    public void OnPointerUp(PointerEventData eventData)
+    {
+        pressed = false; 
+    }
+}
